464d9f45f38d0d29efd3df4e8abab0ad0dfac369
Rajesh Kushwaha <rajeshrsk30@gmail.com>
2025-11-18 13:30:21 +0530

    Update ChatServiceImpl.java

--- server/src/main/java/com/elevate/consultingplatform/service/ai/impl/ChatServiceImpl.java
+++ server/src/main/java/com/elevate/consultingplatform/service/ai/impl/ChatServiceImpl.java
@@ -3,11 +3,13 @@ package com.elevate.consultingplatform.service.ai.impl;
 import com.elevate.consultingplatform.service.ai.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Profile("!live")
 public class ChatServiceImpl implements ChatService {
 
     @Override
